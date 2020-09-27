070ee9f174352ee33bc9b52918c30ba694b44b9a
rakshitraj <rraj.rakshit@gmail.com>
2020-09-27 21:37:59 +0530

    C++ solution , inorder transversal of binary tree

---
+++ 94_btree_inorder_transversal/inorder.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        
+        vector<int> output_arr;
+        stack<TreeNode*> stk;
+        
+        if (root == NULL)   {
+            return output_arr;
+        }
+        
+        TreeNode* current = root;
+        
+        while (current != NULL || !stk.empty())  {
+            while(current != NULL)  {
+                stk.push(current);
+                current = current->left;
+            }
+            
+            current = stk.top();
+            stk.pop();
+            output_arr.push_back(current->val);
+            current = current->right;
+        }  
+               
+        output_arr.shrink_to_fit();
+        
+        return output_arr;
+    }
+};
