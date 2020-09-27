bad0334d1fa6b97bba336b00cfc567c73470191c
rakshitraj <rraj.rakshit@gmail.com>
2020-09-27 21:23:29 +0530

    resolved TLE err, runtime can be improved

--- 2_add_two_numbers/add2.cpp
+++ 2_add_two_numbers/add2.cpp
@@ -24,16 +24,28 @@ public:
         ListNode* dummyHead = new ListNode();
         ListNode* current = dummyHead;
         int carry = 0;
-        int x, y, sum;
-        while ( l1 != nullptr || l2 != nullptr) {
-            x = (l1)? l1->val : 0;
-            y = (l2)? l2->val : 0;
-            sum = carry + x + y;
+        int sum;
+        while (l1 != NULL && l2 != NULL) {
+            sum = carry + l1->val + l2->val;
             carry = sum/10;
-            current->next = new ListNode(sum % 10);
+            current->next = new ListNode(sum%10);
             current = current->next;
-            if (!l1) l1 = l1->next;
-            if (!l2) l2 = l2->next;
+            l1 = l1->next;
+            l2 = l2->next;
+        }
+        ListNode *itr = (l1 == NULL)? l2 : l1;
+        while (itr != NULL) {
+           if (carry > 0){
+               sum = carry + itr->val;
+               carry = sum/10;
+               current->next = new ListNode(sum%10);
+               current = current->next;
+           }
+            else{
+                current->next = new ListNode(itr->val);
+                current = current->next;
+            }
+            itr = itr->next;
         }
         if (carry>0) current->next = new ListNode(carry);
         return dummyHead->next;
