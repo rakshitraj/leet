5d64eec0eba3c9ddb70ddd7e46b3500d99e6d14d
rakshitraj <rraj.rakshit@gmail.com>
2020-09-27 20:38:19 +0530

    C++ solution Err: TIME LIMIT EXCEEDED

--- 2_add_two_numbers/add2.cpp
+++ 2_add_two_numbers/add2.cpp
@@ -8,6 +8,15 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
@@ -16,9 +25,9 @@ public:
         ListNode* current = dummyHead;
         int carry = 0;
         int x, y, sum;
-        while ( l1 != NULL || l2 != NULL) {
-            x = (l1 == NULL)? 0 : l1->val;
-            y = (l2 == NULL)? 0 : l2->val;
+        while ( l1 != nullptr || l2 != nullptr) {
+            x = (l1)? l1->val : 0;
+            y = (l2)? l2->val : 0;
             sum = carry + x + y;
             carry = sum/10;
             current->next = new ListNode(sum % 10);
