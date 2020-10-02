c64d83a045a24793cdaae4b24048f1a5fc0c7893
rakshitraj <rraj.rakshit@gmail.com>
2020-10-03 03:38:50 +0530

    faster solution in python

--- 20_valid_parenthesis/checking_brackets.py
+++ 20_valid_parenthesis/checking_brackets.py
@@ -1,31 +1,16 @@
 from collections import deque
 class Solution:
-    
-    def is_matching(self, left: str, right: str) -> bool:
-        return (left+right) in ['()', '{}', '[]']
-    
     def isValid(self, s: str) -> bool:
-        valid = False
-        stk = deque()
-        
-        if len(s) == 1 :
-            return valid
-        
-        for char in s:
-            if char in '({[':
-                stk.append(char)
-                valid = False
-                
-            if char in ')}]':
-                if len(stk):
-                    if self.is_matching(stk[-1], char):
-                        valid = True
-                        stk.pop()
-                    else:
-                        return False
-                else:
-                    return False
-        
-        if len(stk):
-            return False
-        return valid
+       match = {
+           ')':'(',
+           '}':'{',
+           ']':'['
+       }
+       opened = []
+       for char in s:
+            if match.get(char, False):
+               if len(opened)==0 or opened.pop() != match[char]:
+                   return False
+            else:
+                opened.append(char)
+        return len(opened)==0
