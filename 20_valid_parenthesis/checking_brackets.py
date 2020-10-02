903b0ba975d7d62e26b54d4c51962bfa29a32709
rakshitraj <rraj.rakshit@gmail.com>
2020-10-03 03:40:58 +0530

    faster still

--- 20_valid_parenthesis/checking_brackets.py
+++ 20_valid_parenthesis/checking_brackets.py
@@ -7,10 +7,10 @@ class Solution:
            ']':'['
        }
        opened = []
-       for char in s:
-            if match.get(char, False):
-               if len(opened)==0 or opened.pop() != match[char]:
+       for i in range(len(s)):
+            if match.get(s[i], False):
+               if len(opened)==0 or opened.pop() != match[s[i]]:
                    return False
             else:
-                opened.append(char)
+                opened.append(s[i])
         return len(opened)==0
