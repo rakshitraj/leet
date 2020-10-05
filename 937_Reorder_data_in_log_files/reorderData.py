989c5ffd06f0068929c266733b277a8a4d4b92af
rakshitraj <rraj.rakshit@gmail.com>
2020-10-05 15:41:21 +0530

    faster reorder logs, python

--- 937_Reorder_data_in_log_files/reorderData.py
+++ 937_Reorder_data_in_log_files/reorderData.py
@@ -1,8 +1,13 @@
 class Solution:
     def reorderLogFiles(self, logs: List[str]) -> List[str]:
-        
-        def get_key(log):
-            _id, rest = log.split(" ", maxsplit=1)
-            return (0, rest, _id) if rest[0].isalpha() else (1, )
-        
-        return sorted(logs, key=get_key)
+        digits = []
+        letters = []
+
+        for log in logs:
+            log_split = log.split(' ', maxsplit=1)
+            if log_split[1][0].isdigit():
+                digits.append(log)
+            else:
+                letters.append([''.join(log_split[1]), log_split[0]])
+        letters.sort()
+        return [j+' '+i for i,j in letters] + digits
