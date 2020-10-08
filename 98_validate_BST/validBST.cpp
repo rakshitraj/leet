7fef0c53232eccc96e7704337f07749a78b668a0
rakshitraj <rraj.rakshit@gmail.com>
2020-10-08 16:52:23 +0530

    faster sol. using inorder transversal C++

--- 98_validate_BST/validBST.cpp
+++ 98_validate_BST/validBST.cpp
@@ -11,6 +11,12 @@ struct TreeNode{
 
 class Solution{
 public:
+    
+  bool isValidBST(TreeNode* root) {
+        TreeNode* prev = NULL;
+        return validate(root, prev);
+    }
+    
     /**
      * @brief   Use inorder traversal to check the validity of BST in O(n)
      * @details Traverse the BST in-order and check if it is in ascending order.
@@ -19,18 +25,22 @@ public:
      *          the address of previous element.
      * @param root  Current element
      * @param prev  Previous element std::nullptr by default
-     * @returns bool true if BST is valid
+     * @returns bool, true if Binary search sub-tree is valid
      * 
      */
-    bool isValidBST(TreeNode* root, int* prev= nullptr) {
-        // Exit condition
-        if (!root) return true;
-        // traverse left sub tree(s)
-        if (!validate(root.left, prev)) return false;
-        // Root
-        if (prev != NULL && prev->val >= root.value) return false;
-        // Right sub tree
-        prev = root;
-        return validate(root->right, prev);
+    bool validate(TreeNode* node, TreeNode* &prev) {
+        /* Inorder - left, right  root */
+        /* Exit case */
+        if (node == NULL) return true;
+        
+        /* Left */
+        if (!validate(node->left, prev)) return false;
+        
+        /* Root - remember to save current node as prev for next iteration */
+        if (prev != NULL && prev->val >= node->val) return false;
+        prev = node;
+        
+        /* Right */
+        return validate(node->right, prev);
     }
 };
