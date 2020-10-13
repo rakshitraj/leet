de23cf2e81d29bb0a863f76759d466e37ce08b86
rakshitraj <rraj.rakshit@gmail.com>
2020-10-14 04:00:23 +0530

    faste code, python

--- 53_max_subarray/solution.py
+++ 53_max_subarray/solution.py
@@ -1,23 +1,11 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        maxSum = []
-        currMaxSum = []
-        
-        #Base Case
-        max = nums[0]
-        maxSum.append(max)
-        currMaxSum.append(max)
+        maxSum = nums[0]
+        currSum = nums[0]
         
         for i in range(1, len(nums)):
             current = nums[i]
-            
-            if current + currMaxSum[i-1] > current:
-                currMaxSum.append(current + currMaxSum[i-1])
-            else:
-                currMaxSum.append(current)
-                
-            if currMaxSum[i] > max:
-                max = currMaxSum[i]
-            maxSum.append(max)
-            
-        return maxSum[-1]
+            currSum = max(current, currSum + current)
+            maxSum = max(currSum, maxSum)
+        
+        return maxSum
