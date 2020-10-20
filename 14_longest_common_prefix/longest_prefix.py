712d5d57570844daa99772fa465cc9018c6f491a
rakshitraj <rraj.rakshit@gmail.com>
2020-10-20 16:14:11 +0530

    python 55 percentile solution -- linear traversal of string array

---
+++ 14_longest_common_prefix/longest_prefix.py
@@ -0,0 +1,19 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if len(strs)>0:
+            word = strs[0]
+            for i in range(1, len(strs)):
+                word = self.LCP(word, strs[i]) 
+            return word
+        else:
+            return ''
+    
+    def LCP(self, s1, s2):
+        prefix = ''
+        for i, char in enumerate(s1):
+            if i < len(s2):
+                if char == s2[i] :
+                    prefix += char;
+                else:
+                    break;
+        return prefix
