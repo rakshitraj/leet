a4df2ff7827ef63efe1ec7614981dfce55670bc0
rakshitraj <rraj.rakshit@gmail.com>
2020-10-29 13:16:19 +0530

    100 percentile solution in JAVA

--- 1119_remove_vowels/Solution.java
+++ 1119_remove_vowels/Solution.java
@@ -1,14 +1,18 @@
 class Solution {
     public String removeVowels(String S) {
-        
-        char[] str = S.toCharArray();
-        String output = "";
-        
-        for (int i=0; i<str.length; i++){
-            char ch = str[i];
-            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') continue;
-            output += ch;
+        Set<Character> set = new HashSet<>();
+
+        set.add('a');
+        set.add('e');
+        set.add('i');
+        set.add('o');
+        set.add('u');
+
+        StringBuilder sb = new StringBuilder();
+        for (char c: S.toCharArray()){
+            if (!set.contains(c))
+                sb.append(c);
         }
-        return output;
+        return sb.toString();
     }
 }
