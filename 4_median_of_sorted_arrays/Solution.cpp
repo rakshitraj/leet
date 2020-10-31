374735b1eff8b1794a899d82a7a6008308aa658c
Rakshit Raj <rraj.rakshit@gmail.com>
2020-10-31 14:16:25 +0530

    Commented out redundant fuctions

--- 4_median_of_sorted_arrays/Solution.cpp
+++ 4_median_of_sorted_arrays/Solution.cpp
@@ -45,40 +45,40 @@ private:
         return findMedian(temp);
     }
 
-    void merge(std::vector<int>& arr,std::vector<int>& temp, int left, int mid, int right){
-        if (left > right) return;
+//     void merge(std::vector<int>& arr,std::vector<int>& temp, int left, int mid, int right){
+//         if (left > right) return;
 
-        int i = left;
-        int j = mid+1;
-        int k = left;
+//         int i = left;
+//         int j = mid+1;
+//         int k = left;
 
-        while ((i<=mid) && (j<=right)) {
-            if (arr[i] < arr[j]){
-                temp[k++] = arr[i++]; 
-            }
-            else{
-                temp[k++] = arr[j++];
-            }
-        }
+//         while ((i<=mid) && (j<=right)) {
+//             if (arr[i] < arr[j]){
+//                 temp[k++] = arr[i++]; 
+//             }
+//             else{
+//                 temp[k++] = arr[j++];
+//             }
+//         }
 
-        while (i<= mid) temp[k++] = arr[i++];
-        while (j<= right) temp[k++] = arr[j++];
+//         while (i<= mid) temp[k++] = arr[i++];
+//         while (j<= right) temp[k++] = arr[j++];
 
-        for (k= left; k<=right; k++){
-            arr[k] = temp[k];
-        }
-    }
+//         for (k= left; k<=right; k++){
+//             arr[k] = temp[k];
+//         }
+//     }
 
-    void mergeSort(std::vector<int>& arr, int left, int right) {
-        std::vector<int> temp;
-        temp.reserve(arr.size());
-        temp.assign(arr.size(), 0);
+//     void mergeSort(std::vector<int>& arr, int left, int right) {
+//         std::vector<int> temp;
+//         temp.reserve(arr.size());
+//         temp.assign(arr.size(), 0);
 
-        int mid = (left+right)/2;
-        mergeSort(arr, left, mid);
-        mergeSort(arr, mid+1, right);
-        merge(arr, temp, left, mid, right);
-    }
+//         int mid = (left+right)/2;
+//         mergeSort(arr, left, mid);
+//         mergeSort(arr, mid+1, right);
+//         merge(arr, temp, left, mid, right);
+//     }
 
 public:
     double findMedianSortedArrays(std::vector<int>& nums1, std::vector<int>& nums2) {
