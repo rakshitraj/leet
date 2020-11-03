23b059220181eba6995b24d8ad770da6e9103eb2
Rakshit Raj <rraj.rakshit@gmail.com>
2020-11-03 12:36:09 +0530

    faster solution, c++

--- monthly_challenge/meeting_rooms/solution.cpp
+++ monthly_challenge/meeting_rooms/solution.cpp
@@ -7,16 +7,16 @@ using namespace std;
 class Solution {
 public:
     bool canAttendMeetings(vector<vector<int>>& intervals) {
-        if (intervals.size() == 0) return true;
-        
-        std::sort(intervals.begin(), intervals.end(), [](std::vector<int> a, std::vector<int> b) {
-            return a.front() < b.front();
+        if (intervals.empty()) return true;
+
+        std::sort(intervals.begin(), intervals.end(), [](const vector<int>& a, const vector<int>& b) {
+            return a[0] < b[0];
         });
 
-        for (int i=0; i < intervals.size()-1; i++) {
-            if (intervals[i].back() > intervals[i+1].front())
-                return false; 
+        for (int i=0; i < intervals.size()-1; i++){
+            if (intervals[i][1] > intervals[i+1][0])
+                return false;
         }
         return true;
-    }
+        }
 };
