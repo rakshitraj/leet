6d6068d1f470a8afc6693ea95d3c9bb4cd22f0ec
Rakshit Raj <rraj.rakshit@gmail.com>
2020-11-05 15:54:26 +0530

    fastest soution

--- monthly_challenge/move_chips/solution.cpp
+++ monthly_challenge/move_chips/solution.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <vector>
-#include <map>
 
 using namespace std;
 
@@ -8,22 +7,12 @@ class Solution {
 public:
     int minCostToMoveChips(vector<int>& position) {
         
-        std::map<int, int> costs;
-        int minCost = INT32_MAX;
-
-        for(int destination: position){
-            if (costs.find(destination) == costs.end())
-            {
-                int cost = 0;
-                for (int i=0; i<position.size(); i++){
-                    cost += abs(position[i] - destination)%2;
-                }
-                costs.insert(std::make_pair(destination, cost));
-            }
-            if (costs[destination] < minCost){
-                minCost = costs[destination];
-            }
-        }
-        return minCost;
+       int evenPos = 0;
+       int oddPos = 0;
+       for (int i=0; i<position.size(); i++){
+           if (position[i]%2 == 0) evenPos++;
+           else oddPos++;
+       }
+       return std::min(evenPos, oddPos);
     }
 };
