5d34f4564673d6908bcfebdbaf2d814d73305499
Rakshit Raj <rraj.rakshit@gmail.com>
2020-11-08 19:58:47 +0530

    solution in C++

---
+++ 563_binary_tree_tilt/solution.cpp
@@ -0,0 +1,81 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int val) : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int val, TreeNode *left, TreeNode *right) : val(val), left(left), right(right) {}
+};
+
+class Solution {
+    
+    int helper(TreeNode *root, int &sum)
+    {
+        if(!root)
+        {
+            sum = 0;
+            return 0;
+        }
+        int lSum, rSum, l, r;
+        l = helper(root->left, lSum);
+        r = helper(root->right, rSum);
+        sum = lSum + rSum + root->val;
+        return abs(lSum - rSum) + l + r;
+    }
+    
+public:
+    int findTilt(TreeNode* root) {
+        int sum;
+        return helper(root, sum);
+    }
+};
+
+// class Solution {
+//     int sumSubTree (TreeNode* root){
+//         int sum = 0;
+//         if (root != NULL){
+//             sum+= (root->val + sumSubTree(root->left) + sumSubTree(root->right));
+//         }
+//         return sum;
+//     }
+
+//     void get_tilt(TreeNode* root, vector<int>& allTilt){
+
+//         if (root != NULL){
+//         int tilt = abs(sumSubTree(root->left) - sumSubTree(root->right));
+//         get_tilt(root->left, allTilt);
+//         get_tilt(root->right, allTilt);
+//         allTilt.push_back(tilt);
+//         }
+//     }
+
+// public:
+//     int findTilt(TreeNode* root) {
+
+//         int sumAllTilt = 0;
+//         std::vector<int> allTilt; 
+//         get_tilt(root, allTilt);
+
+//         for (int el: allTilt){
+//             sumAllTilt += el;
+//         }
+
+//         return sumAllTilt;
+        
+//     }
+// };
