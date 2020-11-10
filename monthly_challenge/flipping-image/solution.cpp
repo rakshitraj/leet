7febebcbe3c867f1149a5dc3882bed2266b9dff0
Rakshit Raj <rraj.rakshit@gmail.com>
2020-11-10 13:55:02 +0530

    solution in C++

--- monthly_challenge/flipping-image/solution.cpp
+++ monthly_challenge/flipping-image/solution.cpp
@@ -5,27 +5,16 @@
 using namespace std;
 
 class Solution {
-    void reverse(vector<vector<int>>& flipped){
-        for (auto row: flipped){
-            std::reverse(row.begin(), row.end());
-        }
-    }
-
-    void invert(vector<vector<int>>& flipped){
-        for (auto row: flipped){
-            for (int i= 0; i<flipped.size(); i++){
-                if (row[i] == 0) row[i] = 1;
-                else row[i] = 0;
-            }
-        }
-    }
 public:
     vector<vector<int>> flipAndInvertImage(vector<vector<int>>& A) {
         if (A.empty()) return vector<vector<int>>{};
 
-        vector<vector<int>> flipped;
-        reverse(A);
-        invert(A);
-        return A;
+        vector<vector<int>> flipped(A.size());
+        for (int i=0; i<A.size(); i++) {
+            for (int j=A[i].size()-1; j>=0; j--) {
+                flipped[i].push_back((A[i][j] == 0 ? 1 : 0));
+            }
+        }
+        return flipped;
     }
 };
