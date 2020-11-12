4414544b6e5b235a7b5971ab8465204a18fbf3d9
Rakshit Raj <rraj.rakshit@gmail.com>
2020-11-12 21:36:30 +0530

    solution using std lib function in C++

---
+++ monthly_challenge/permutations-II/solution.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> permutation;
+        
+        do {
+            permutation.push_back(nums);
+        }
+        while(next_permutation(nums.begin(), nums.end()));
+        return permutation;
+    }
+};
