38f8923301cc9b2e2668a68b8f85c5ed0d4f16f6
Rakshit Raj <rraj.rakshit@gmail.com>
2020-11-15 22:16:50 +0530

    solution

--- monthly_challenge/remove-interval/solution.cpp
+++ monthly_challenge/remove-interval/solution.cpp
@@ -10,25 +10,21 @@ public:
         std::vector<std::vector<int>> result;
 
         for (auto el: intervals) {
-
-            if (el[1] <= toBeRemoved[0]){
+            if (el[1] <= toBeRemoved[0] || el[0] >= toBeRemoved[1]) {
                 result.push_back(el);
             }
-            else if (el[0] >= toBeRemoved[1]) {
-                result.push_back(el);
+            else if (el[0] >= toBeRemoved[0] && el[1] <= toBeRemoved[1]){
+                continue;
             }
-            else if(el[0] > toBeRemoved[0] && el[1] <= toBeRemoved[1]) continue;
             else {
-                int start, end;
-                if (el[1] < toBeRemoved[1]) {
-                    start = el[0];
-                    end = toBeRemoved[0];
+                if (el[0] < toBeRemoved[0] && el[1] >= toBeRemoved[1]) {
+                    result.push_back(std::vector<int>{el[0], toBeRemoved[0]});
+                    result.push_back(std::vector<int>{toBeRemoved[1], el[1]});
                 }
-                else {
-                    start = toBeRemoved[1];
-                    end = el[1];
+                else if (el[0] < toBeRemoved[0] && el[1] < toBeRemoved[1]) {
+                    result.push_back(std::vector<int>{el[0], toBeRemoved[0]});
                 }
-                result.push_back(std::vector<int>{start, end});
+                else result.push_back(std::vector<int>{toBeRemoved[1], el[1]});
             }
         }
         return result;
