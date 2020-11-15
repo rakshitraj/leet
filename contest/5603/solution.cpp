b035185b5e40d736f73b7c3bab1679767ef77af5
Rakshit Raj <rraj.rakshit@gmail.com>
2020-11-15 10:23:58 +0530

    weekly contest 215

--- contest/5603/solution.cpp
+++ contest/5603/solution.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <cstring>
-#include <set>
+#include <unordered_set>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -8,24 +10,35 @@ class Solution {
 public:
     bool closeStrings(string word1, string word2) {
         
-        std::set<char> w1;
-        std::set<char> w2;
+        if (word1.length() != word2.length()) return false;
         
+        std::unordered_set<char> w1;
+        std::unordered_set<char> w2;
+        std::vector<int> one(26, 0);
+        std::vector<int> two(26, 0);
+
+        one.resize(26);
+
+               
         for (char ch: word1){
             w1.insert(ch);
+            one[(int)ch - 97]++;
         }
+        std::sort(one.begin(), two.begin());
+
         for (char ch: word2) {
+            if (w1.insert(ch).second) return false;
             w2.insert(ch);
+            two[(int)ch - 97]++;
         }
+        std::sort(two.begin(), two.begin());
+        
+        if(w1.size() != w2.size()) return false;
 
-        if (w1.size() != w2.size()) return false;
-
-        std::set<char>::iterator it1 = w1.begin();
-        std::set<char>::iterator it2 = w2.begin();
-
-        while(it1 != w1.end()){
-            if (*it1 != *it2) return false;
+        for (int i=0; i<26; i++) {
+            if (one[i] != two[i]) return false;
         }
+        
         return true;
     }
 };
